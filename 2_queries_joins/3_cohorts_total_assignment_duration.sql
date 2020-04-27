2bc0415e755722cc010dab9daa7b9ce6ef0e87a5
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-27 13:49:31 -0700

    added joins for single student and cohorts

---
+++ 2_queries_joins/3_cohorts_total_assignment_duration.sql
@@ -0,0 +1,7 @@
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM assignment_submissions
+JOIN students
+ON students.id = student_id
+JOIN cohorts
+ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
