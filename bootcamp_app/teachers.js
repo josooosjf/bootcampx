7cb5a796aa3d476886155bd3da9f3c74e980961d
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-28 19:10:00 -0700

    parametized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
   FROM assistance_requests
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+  WHERE cohorts.name = $1
   ORDER BY teachers.name;
-  `)
+  `;
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [cohortName];
+
+pool.query(queryString, values)
+
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort} : ${user.teacher}`);
